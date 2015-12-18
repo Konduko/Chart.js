7ca59d5624c2a90db74597483f057bab7510c648
Shane Jennings <shane.jennings@piksel.com>
2015-12-18 10:22:34 -0500

    updating some package metadata

--- meteor/package.js
+++ meteor/package.js
@@ -1,20 +1,20 @@
 // package metadata file for Meteor.js
 'use strict';
 
-var packageName = 'chart:chart';  // https://atmospherejs.com/chart/chart
+var packageName = 'konduko:chartjs';  // https://atmospherejs.com/chart/chart
 var where = 'client';  // where to install: 'client' or 'server'. For both, pass nothing.
 
 var packageJson = JSON.parse(Npm.require("fs").readFileSync('package.json'));
 
 Package.describe({
   name: packageName,
-  summary: 'Chart.js (official) - Responsive, simple, clean and interactive charts using HTML5 <canvas>',
+  summary: 'Chart.js (konduko) - Responsive, simple, clean and interactive charts using HTML5 <canvas>',
   version: packageJson.version,
-  git: 'https://github.com/MeteorPackaging/Chart.js.git'
+  git: 'https://github.com/Konduko/Chart.js.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']);
+  api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.2.1']);
   api.export('Chart');
   api.addFiles([
     'Chart.js',
